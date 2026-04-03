8937349f55c770c46b70f36419b7142d6b734cd1
chandhru2008 <gschandhru10@gmail.com>
2026-04-03 23:06:01 +0530

    feat(auth): implement Google login using @react-oauth/google

---
+++ app/src/components/GoogleAuth/GoogleLoginButton.tsx
@@ -0,0 +1,16 @@
+import { GoogleLogin } from '@react-oauth/google';
+
+const GoogleLoginButton = () => {
+  return (
+    <GoogleLogin
+      onSuccess={(credentialResponse) => {
+        console.log(credentialResponse);
+      }}
+      onError={() => {
+        console.log('Login Failed');
+      }}
+    />
+  );
+};
+
+export default GoogleLoginButton;
